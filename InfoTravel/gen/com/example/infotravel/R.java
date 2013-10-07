85b9a93bb093c369654adbd962fa6c15a3fd482f
jdarjona <jdarjona@hotmail.com>
2013-10-07 16:25:36 +0200

    otro 2

--- InfoTravel/gen/com/example/infotravel/R.java
+++ InfoTravel/gen/com/example/infotravel/R.java
@@ -29,9 +29,9 @@ public final class R {
         public static final int button1=0x7f080004;
         public static final int city=0x7f080000;
         public static final int listView1=0x7f080005;
-        public static final int nota=0x7f080003;
+        public static final int nota=0x7f080002;
         public static final int pais=0x7f080001;
-        public static final int year=0x7f080002;
+        public static final int year=0x7f080003;
     }
     public static final class layout {
         public static final int activity_edit=0x7f030000;
